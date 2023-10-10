e9ecc82ea6cb3982b71b9e4ef0bf6a546986c28e
v_yyyiichen <v_yyyiichen@tencent.com>
2023-10-10 11:15:01 +0800

    添加多线程获取雪花算法id和分段id 用于排查是否存在重复的id

--- src/test/java/com/wuhao/TestController.java
+++ src/test/java/com/wuhao/TestController.java
@@ -1,17 +1,27 @@
 package com.wuhao;
 
 
+import com.sankuai.inf.leaf.common.Result;
+import com.sankuai.inf.leaf.common.Status;
+import com.sankuai.inf.leaf.service.SnowflakeService;
 import com.wuhao.dao.UserMapper;
 import com.wuhao.domain.User;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.annotation.Resource;
+
+@Slf4j
 @SpringBootTest
 public class TestController {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Resource
+    private SnowflakeService snowflakeService;
 
     @Test
     public void testAdd(){
@@ -23,4 +33,16 @@ public class TestController {
         //user.setUserId(IDUtil.getRandomId());
         userMapper.addUser(user);
     }
+
+
+    public void getSnowflakeId(){
+        Result result = snowflakeService.getId("id");
+        if (result.getStatus().equals(Status.EXCEPTION)) {
+            log.error("error |{}",result);
+        }
+        log.info("result Id | {}",String.valueOf(result.getId()));
+
+    }
+
+
 }
