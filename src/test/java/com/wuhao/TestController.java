ce88215af28063b3f154f1337b2f648796d0f4b8
v_yyyiichen <v_yyyiichen@tencent.com>
2023-10-13 11:32:10 +0800

    feat:添加springboot 整合log4j2日志输出至本地文件

--- src/test/java/com/wuhao/TestController.java
+++ src/test/java/com/wuhao/TestController.java
@@ -3,22 +3,29 @@ package com.wuhao;
 
 import com.sankuai.inf.leaf.common.Result;
 import com.sankuai.inf.leaf.common.Status;
+import com.sankuai.inf.leaf.service.SegmentService;
 import com.sankuai.inf.leaf.service.SnowflakeService;
 import com.wuhao.dao.UserMapper;
 import com.wuhao.domain.User;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
 @Slf4j
 @SpringBootTest
+@RunWith(SpringRunner.class)
 public class TestController {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Resource
+    private SegmentService segmentService;
 
     @Resource
     private SnowflakeService snowflakeService;
@@ -35,6 +42,7 @@ public class TestController {
     }
 
 
+    @Test
     public void getSnowflakeId(){
         Result result = snowflakeService.getId("id");
         if (result.getStatus().equals(Status.EXCEPTION)) {
